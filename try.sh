41723d5af3b0dc77926e01aef5eb890864e6befa
prabhatigauda <90883889+prabhatigauda@users.noreply.github.com>
2021-09-19 11:56:27 +0530

    Delete try.sh

--- try.sh
+++
@@ -1,9 +0,0 @@
-Dictionary()
-{
- declare -A dict=([A_day9.sh]= A_print)
- echo "([${dict[@]}]=${dict[@]})"
-}
-declare -A output = "$(Dictionary)"
-echo day9.sh : ${output[@]}
-echo value : ${output[@]}
-
