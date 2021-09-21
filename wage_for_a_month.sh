421742d76621f6396228b512c17f7f2ad1bc11f5
prabhatigauda <90883889+prabhatigauda@users.noreply.github.com>
2021-09-21 22:47:03 +0530

    Delete wage_for_a_month.sh

--- wage_for_a_month.sh
+++
@@ -1,6 +0,0 @@
-wagePerHour=20
-fullDayHour=8
-workingDaysPerMonth=20
-Salary_for_a_day=$((fullDayHour*wagePerHour))
-Salary_for_a_Month=$((Salary_for_a_day*workingDaysPerMonth))
-echo "Month Salary : $Salary_for_a_Month"
